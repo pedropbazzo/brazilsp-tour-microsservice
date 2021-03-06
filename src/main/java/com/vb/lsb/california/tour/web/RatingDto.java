7a2b62e2df511210a4ef71954abc3addeae78aa7
Pedro Paulo Bazzo <pedropbazzo@outlook.com>
2021-03-06 12:23:59 -0300

    add: new feat tour brazil sp

---
+++ src/main/java/com/vb/lsb/california/tour/web/RatingDto.java
@@ -0,0 +1,68 @@
+package com.vb.lsb.california.tour.web;
+
+import org.springframework.hateoas.ResourceSupport;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Data Transfer Object for Rating a Tour.
+ *
+ * @author Vaibhav Gupta
+ */
+
+public class RatingDto extends ResourceSupport {
+
+    @Min(0)
+    @Max(5)
+    private Integer score;
+
+    @Size(max = 255)
+    private String comment;
+
+    @NotNull
+    private Integer customerId;
+
+    /**
+     * Constructor to fully initialize the RatingDto
+     *
+     * @param score      score
+     * @param comment    comment
+     * @param customerId customer identifier
+     */
+    public RatingDto(Integer score, String comment, Integer customerId) {
+        this.score = score;
+        this.comment = comment;
+        this.customerId = customerId;
+    }
+
+    protected RatingDto() {
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+}
